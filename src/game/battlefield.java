35cfbae29164e760dfd0605b1728ff92e60ec11e
franclocat <148209747+franclocat@users.noreply.github.com>
2023-12-07 15:25:53 +0100

    Working with length

--- src/game/battlefield.java
+++ src/game/battlefield.java
@@ -1,7 +1,5 @@
 package game;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class battlefield {
@@ -9,7 +7,7 @@ public class battlefield {
     public static void main(String[] args) {
         String[][] board = createBoard();
         printBoard(board);
-        promptCoordinates();
+        getBoatCoordinates();
     }
     private static String[][] createBoard() {
         String[][] board = new String[10][10];
@@ -40,17 +38,50 @@ public class battlefield {
 
     }
 
-    private static StringBuilder promptCoordinates() {
-        System.out.println("Enter the coordinates of the ship (just the start and end):");
+    private static StringBuilder getBoatCoordinates() throws NullPointerException{
         Scanner scanner = new Scanner(System.in);
-        StringBuilder coordinates = new StringBuilder();
-        for (int i = 0; i < 2; i++) {
-            if (scanner.hasNext()) {
-                coordinates.append(scanner.next());
+        boolean valid = false;
+        StringBuilder firstCoords = new StringBuilder();
+        StringBuilder secondCoords = new StringBuilder();
+
+        //Make a loop that keeps prompting for coordinates if the input given is not valid
+        while (!valid) {
+            System.out.println("Enter the coordinates of the ship (start and end):");
+            firstCoords.append(scanner.next());
+            secondCoords.append(scanner.next());
+
+            //check for a valid length for the input without knowing if the given values are valid
+            if (firstCoords.length() < 2 || firstCoords.length() > 3 || secondCoords.length() < 2 || secondCoords.length() > 3) {
+                System.out.println("Error: The input has to look like \"A5 A10\"!\n");
+                continue;
             }
+            //divide the coordinates in rows and columns for later validation
+            String rowFirst = String.valueOf(firstCoords.charAt(0));
+            String rowSecond = String.valueOf(secondCoords.charAt(0));
+            String colFirst = String.valueOf(firstCoords.substring(1,firstCoords.length()));
+            String colSecond = String.valueOf(secondCoords.substring(1,secondCoords.length()));
+
+            //define valid values for rows
+            String validRows = "ABCDEFGHIJ";
+
+            if (validRows.contains(rowFirst) && validRows.contains(rowSecond) && colFirst.matches("[1-9]|10") &&
+            colSecond.matches("[1-9]|10")) {
+                if (rowFirst.equals(rowSecond) && !colFirst.equals(colSecond)) {
+                     int sum = Math.abs(Integer.parseInt(colFirst) - Integer.parseInt(colSecond)) + 1;
+                     System.out.println("Length: " + sum);
+                     valid = true;
+                } else if (colFirst.equals(colSecond) && !rowFirst.equals(rowSecond)) {
+                    int sum = Math.abs(firstCoords.charAt(0) - secondCoords.charAt(0)) + 1;
+                    System.out.println("Length: " + sum);
+                    valid = true;
+                }
+            } else {
+                System.out.println("Error: The coordinates have to be between [A-J] and [1-10]!");
+            }
+
+
         }
-        System.out.println(coordinates);
-        return coordinates;
+        return firstCoords;
     }
 
 }
