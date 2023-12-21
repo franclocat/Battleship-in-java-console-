1971f5f69d3cd0bebf29e89a72bb5a5713711617
franclocat <148209747+franclocat@users.noreply.github.com>
2023-12-21 02:40:30 +0100

    Classes update (clean code)

--- src/game/Main.java
+++ src/game/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Player player1 = new Player();
         Player player2 = new Player();
+
+        Player.printBoard(player1.playerBoard);
+        player1.placeShips();
+        player1.shootTheShips();
     }
 }
--- src/game/Ship.java
+++ src/game/Ship.java
@@ -5,10 +5,35 @@ import java.util.List;
 public class Ship {
     String name;
     int length;
+    String direction;
     List<List<int[]>> segments;
 
-    public Ship(String name, List<List<int[]>> segments) {
+    public Ship(String name, int length) {
         this.name = name;
-        this.segments = segments;
+        this.length = length;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
     }
 }
