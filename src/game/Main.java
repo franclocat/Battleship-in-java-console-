9d156959e0a81fcc17b972852b484617866cbfcc
franclocat <148209747+franclocat@users.noreply.github.com>
2023-12-21 01:44:31 +0100

    a

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,5 +1,8 @@
 package game;
 
 public class Main {
-
+    public static void main(String[] args) {
+        Player player1 = new Player();
+        Player player2 = new Player();
+    }
 }
--- src/game/Ship.java
+++ src/game/Ship.java
@@ -5,28 +5,10 @@ import java.util.List;
 public class Ship {
     String name;
     int length;
-    List <List <int[]>> segments;
+    List<List<int[]>> segments;
 
-    public Ship (String name, int length) {
+    public Ship(String name, List<List<int[]>> segments) {
         this.name = name;
-        this.length = length;
-    }
-
-    public int getLength() {
-        return length;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setLength(int length) {
-        this.length = length;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setSegments(List<List<int[]>> segments) {
         this.segments = segments;
     }
 }
